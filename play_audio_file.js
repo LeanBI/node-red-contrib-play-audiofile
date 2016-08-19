aa99de9476d58f306a79ba65b6aeae201abaff2e
sbrennion <sebastien.brennion@leanbi.ch>
2016-08-19 11:26:59 +0200

    Initial commit

---
+++ play_audio_file.js
@@ -0,0 +1,36 @@
+module.exports = function(RED) {
+
+    function Play_audio_file(config) {
+        this.log("test")
+    	RED.nodes.createNode(this,config);
+        var node = this;
+        //this.status({fill:"red",shape:"dot",text:"not started"});
+
+
+        this.on("input",function(msg){
+            f=config.filename || msg.filename
+            var SoundPlayer = require('soundplayer')
+            var player=new SoundPlayer();
+
+              // play with a callback
+              player.sound(f, function(){
+                node.log('playing ' + f)
+              });
+
+              player.on('error', function (error) {
+                node.log("unable to play sound : " + f + "\n" + JSON.stringify(error))
+              });
+
+
+        })
+
+
+
+    }
+
+
+
+
+
+    RED.nodes.registerType("play_audio_file",Play_audio_file);
+}
---
+++ readme.md
@@ -0,0 +1,15 @@
+#requirements
+
+See https://www.npmjs.com/package/soundplayer
+
+One of the CLI based audio player
+
+'mpg123'
+'mpg321'
+'play'
+(anyother cli based audio player)
+
+
+#usage
+
+specify the audio file name to play in the config or in the message header : msg.filename=/home/me/file.mp3
